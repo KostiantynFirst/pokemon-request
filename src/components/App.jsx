ceed8237283c9234ab70e96792447d39871a5548
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-06-20 11:15:14 +0300

    s

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from "react";
+import {ToastContainer } from 'react-toastify';
+export default class App extends Component {
+
+  render() {
+    return (
+      <div style={{maxWidth: 1170, margin: '0 auto', padding: 20}}>
+        <ToastContainer/>
+      </div>
+    );
+  }
+}
