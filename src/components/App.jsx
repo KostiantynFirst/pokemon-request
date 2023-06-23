5425a6657447b9979e895d426f6dadcb1e28ec53
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-06-23 10:19:48 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,23 @@
 import { Component } from "react";
-import {ToastContainer } from 'react-toastify';
+// import { ToastContainer } from 'react-toastify';
+
+
 export default class App extends Component {
+
+  state = {
+    pokemon: null
+  }
+
+  componentDidMount() {
+    fetch(`https://pokeapi.co/api/v2/pokemon/ditto`)
+      .then(res => res.json())
+      .then(console.log);
+  }
 
   render() {
     return (
       <div style={{maxWidth: 1170, margin: '0 auto', padding: 20}}>
-        <ToastContainer/>
+        {this.state.pokemon && <div>This will be pokemon</div>}
       </div>
     );
   }
