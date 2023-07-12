8fa93a280705922eb5cd7d02fbc265c8bdcd4a45
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-07-12 10:01:13 +0300

    sum

---
+++ src/components/PokemonForm.js
@@ -0,0 +1,39 @@
+import { Component } from 'react'; 
+import { ImSearch } from 'react-icons';
+
+const styles = { form: { marginBottom: 20 }};
+
+export default class PokemonForm extends Component {
+    state = {
+        pokemonName: '',
+    }
+
+    handleNameChange = e => {
+        this.setState({ pokemonName: e.currentTarget.value.toLowerCase() });
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+
+        this.props.onSubmit(this.state.pokemonName);
+        this.setState({ pokemonName: '' })
+    };
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit} style={styles.form}>
+                <input 
+                    type="text"
+                    name="pokemonName"
+                    value={this.state.pokemonName}
+                    onChange={this.handleNameChange}
+                />
+                <button type="submit">
+                    <ImSearch style={{ marginRight: 8 }}/>
+                    Search
+                </button>
+
+            </form>
+        )
+    }
+}
